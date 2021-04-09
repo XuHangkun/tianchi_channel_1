b81bff325c62f4fe919cc19770d15bb6fe62f00d
Hangkun Xu <xuhangkun@ihep.ac.cn>
2021-04-09 17:35:53 +0800

    fix train.sh

--- code/train.sh
+++ code/train.sh
@@ -15,8 +15,8 @@ python train.py -model DPCNN -eda_alpha 0.1 -n_aug 0.5 -epoch 8 -no_word2vec_pre
 -save_per_epoch 1
 
 # 5-fold train BERT
-python train.py -model BERT_PLoss0.64 -epoch 30 \
+python train.py -model BERT_PLoss0.64 -epoch 30 -no_word2vec_pretrain\
 -bert_path /hpcfs/juno/junogpu/xuhangkun/ML/MyselfProject/tianchi_channel_1/user_data/bert/checkpoint-15800 \
--save_mode all -save_start_epoch 10 -save_per_epoch 3 -fold_index 5 \
+-save_mode all -save_start_epoch 10 -save_per_epoch 3 \
 -output_dir /hpcfs/juno/junogpu/xuhangkun/ML/MyselfProject/tianchi_channel_1/user_data/model_data/Bert_new \
 -lr 5.e-5
