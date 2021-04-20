b1866268adea669f71d67ffeaa42679be78ca40b
Xu Hangkun <xuhangkun@LAPTOP-RBBUESOS.localdomain>
2021-04-20 11:57:12 +0800

    fix bug about TextMRCNN prediction

--- code/predict.py
+++ code/predict.py
@@ -49,7 +49,7 @@ def load_model(opt,device):
         elif  "TextMRCNN_" in opt.models[index]:
             model_config = TextMRCNNConfig(n_vocab=model_setting.ntokens,embedding=model_setting.nemb,max_seq_len=60,num_class=model_setting.nclass,dropout=model_setting.dropout)
             model_config.padding_idx = model_setting.pad_token
-            m_model = TextRCNNModel(model_config).to(device)
+            m_model = TextMRCNNModel(model_config).to(device)
         elif "TextRNNAtt_" in opt.models[index]:
             model_config = TextRNNAttConfig(model_setting.ntokens,model_setting.nemb,model_setting.nclass)
             model_config.padding_idx = model_setting.pad_token
