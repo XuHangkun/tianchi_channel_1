bbee4e0aee4522fe0a9bf6cb82cc46c6eb6717b6
Hangkun Xu <xuhangkun@ihep.ac.cn>
2021-04-23 20:58:25 +0800

    tune word to vector script

--- code/train_word_vector.py
+++ code/train_word_vector.py
@@ -13,48 +13,57 @@ import gensim
 import pandas as pd
 import torch
 import argparse
-from glove import Glove
-from glove import Corpus
+# from glove import Glove
+# from glove import Corpus
 import pickle
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-word_size',default=250,type=int,help="dimension of a word vector")
+parser.add_argument('-word_size',default=300,type=int,help="dimension of a word vector")
+parser.add_argument('-epoch',type=int,default=100,help="epoch")
+parser.add_argument('-batch_size',type=int,default=128,help="epoch")
+parser.add_argument('-corpus_dir',default=os.path.join(os.getenv('PROJTOP'),'tcdata'),help="dir of corpus")
 parser.add_argument('-output',default=os.path.join(os.getenv('PROJTOP'),'user_data/word_pretrain/word2vector.model'),help="dimension of a word vector")
 opt = parser.parse_args()
 
 # read csv data
 print('read data and preprocess the data')
-train_df = pd.read_csv(os.path.join(os.getenv('PROJTOP'),'tcdata/medical_nlp_round1_data/train.csv'),sep="\|,\|",names=["id","report","label"],index_col=0)
 reports = []
-for index in range(len(train_df)):
-    report = train_df['report'][index].split()
-    reports.append(report)
+corpus_input = ["track1_round1_train_20210222.csv","track1_round1_testA_20210222.csv","track1_round1_testB.csv","train.csv"]
+corpus_input_tag = [0,1,1,0]
+for corpus_file,tag in zip(corpus_input,corpus_input_tag):
+    if tag:
+        train_df = pd.read_csv(os.path.join(opt.corpus_dir,corpus_file),sep="\|,\|",names=["id","report"],index_col=0)
+    else:
+        train_df = pd.read_csv(os.path.join(opt.corpus_dir,corpus_file),sep="\|,\|",names=["id","report","label"],index_col=0)
+    for i in range(len(train_df)):
+        reports.append(train_df["report"][i].split())
+# print(reports)
 
 # Train the word2vec model
 print('train the word2vec model')
 model = gensim.models.Word2Vec(reports, min_count=1,size=opt.word_size,hs=1)
-model.train(epochs=100)
+model.train(reports,epochs=100,total_examples=len(reports))
 print(model.wv)
 
 # Train Glovec
-print('train the glovec model')
-corpus_model = Corpus()
-corpus_model.fit(reports, window=6)
-glove = Glove(no_components=opt.word_size, learning_rate=0.05)
-glove.fit(corpus_model.matrix, epochs=100,
-          no_threads=1, verbose=True)
-glove.add_dictionary(corpus_model.dictionary)
+# print('train the glovec model')
+# corpus_model = Corpus()
+# corpus_model.fit(reports, window=6)
+# glove = Glove(no_components=opt.word_size, learning_rate=0.05)
+# glove.fit(corpus_model.matrix, epochs=100,
+#           no_threads=1, verbose=True)
+# glove.add_dictionary(corpus_model.dictionary)
 
 # cancate the two word size
 info = {
-        "word_size":2*opt.word_size,
+        "word_size":opt.word_size,
         "wv":{
             }
         }
 for word in model.wv.vocab:
-    info["wv"][word] = torch.cat((torch.Tensor(model.wv[word]),torch.Tensor(glove.word_vectors[glove.dictionary[word]])),0)
-    print(info["wv"][word].shape)
+    info["wv"][word] = torch.Tensor(model.wv[word])
+    print(info["wv"][word])
 
 f = open(opt.output,'wb')
 pickle.dump(info,f)
